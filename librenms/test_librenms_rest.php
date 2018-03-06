e26d18afc02386e0372f9b0f79c01f3dfffa9df9
zorille <zorille@free.fr>
2018-03-06 15:03:19 +0000

    Ajout du help

--- librenms/test_librenms_rest.php
+++ librenms/test_librenms_rest.php
@@ -15,6 +15,42 @@ $rep_document = dirname ( $argv [0] ) . "/../..";
  * Permet d'inclure toutes les librairies communes necessaires
  */
 require_once $rep_document . "/php_framework/config.php";
+/**
+ *
+ * @ignore Affiche le help.<br>
+ *         Cette fonction fait un exit.
+ *         Arguments reconnus :<br>
+ *         --help
+ */
+function help() {
+	$fichier = basename ( __FILE__ );
+	$help = array (
+			"usage" => array (
+					$fichier . " --conf [fichiers de conf] [OPTIONS]",
+					$fichier . " --help"
+			),
+			$fichier => array ()
+	);
+	$help [$fichier] ["text"] = array ();
+	$help [$fichier] ["text"] [] .= "./test_librenms_rest.php --conf ../../conf_clients/librenms/dev_client_librenms_serveurs.xml --librenms_serveur librenms-test --librenms_token XXXXxxxXXXX --verbose";
+	$help [$fichier] ["text"] [] .= "\t--librenms_serveurr Nom du server dans le fichier de configuration";
+	$help [$fichier] ["text"] [] .= "\t--librenms_token Token fournit par LibreNMS";
+	
+	
+	$class_utilisees = array (
+			"fichier","librenms_wsclient","librenms_datas"
+	);
+	$help = array_merge ( $help, fonctions_standards::help_fonctions_standard ( false, true, $class_utilisees ) );
+	fonctions_standards::affichage_standard_help ( $help );
+	echo "[Exit]0\n";
+	exit ( 0 );
+}
+
+// Cette fonction fait un exit 0
+if ($liste_option->verifie_option_existe ( "help" ))
+	help ();
+	
+
 // ==================================================
 // Etape 2 : Recuperation des arguments du scripts
 // ==================================================
