5d1348559f2f9199b2798ba4a93da0337ddd957c
Damien V <d.vargas@infraops.fr>
2018-09-21 09:51:18 +0100

    Reprise des Switch et DVSwith avec mise a jour des PortGroup

---
+++ itop/plugins/DVSwitch/en.dict.DVSwitch.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:DVSwitch' => 'Distributed VSwitch',
+	'Class:DVSwitch+' => 'Distributed VSwitch',
+	'DVSwitch:baseinfo' => 'General information',
+	'DVSwitch:VirtualInfo' => 'VCenter Host',
+	'Class:DVSwitch/Attribute:virtualmachine_id' => 'vCenter',
+	'Class:DVSwitch/Attribute:virtualmachine_id+' => 'Mandatory in DVSwitch mode',
+	'Class:VCenter/Attribute:dvswitchs_list' => 'DVSwitchs List',
+	'Class:VCenter/Attribute:dvswitchs_list+' => 'DVSwitchs connected to vCenter',
+));
+?>
---
+++ itop/plugins/VCenter/en.dict.VCenter.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:VCenter' => 'vCenter',
+));
+?>
