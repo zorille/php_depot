01f98412102b610f87f0b22024e05670128c8591
Damien Vargas <zorille@free.FR>
2018-12-03 14:58:50 +0000

    Add Dante OPNSense's package

---
+++ opnsense/dante/src/www/diag_logs_dante.php
@@ -0,0 +1,18 @@
+<?php
+require_once("guiconfig.inc");
+
+function build_logfile_list() {
+	$config = &config_read_array('OPNsense','dante','sockdglobal');
+	
+	if (is_array($config) && isset($config['global']['logOutput'])) {
+		return $config['global']['logOutput'];
+	}
+	return 'sockd.log';
+}
+
+$logfile = '/var/log/Dante_opnsense/'.build_logfile_list();
+$logclog = false;
+
+$service_hook = 'dante';
+
+require_once 'diag_logs_template.inc';
