fb002379d7a27790c9d882c35145c0ae9b6ec765
Damien Vargas <zorille@free.FR>
2019-09-06 18:48:33 +0100

    Remise a jour interface sockdiops OPNSense

--- opnsense/sockdiops/src/opnsense/service/templates/OPNsense/SockdIOPS/rc.conf.d
+++ opnsense/sockdiops/src/opnsense/service/templates/OPNsense/SockdIOPS/rc.conf.d
@@ -2,6 +2,7 @@
 sockdiops_var_script="/usr/local/opnsense/scripts/OPNsense/SockdIOPS/setup.sh"
 sockdiops_config="/usr/local/etc/SockdIOPS/sockdiops.conf"
 sockdiops_enable="YES"
+sockdiops_flags=""
 {% else %}
 sockdiops_enable="NO"
 {% endif %}
