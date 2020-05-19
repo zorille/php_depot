534f83c5ebdb05c9cd77619e18f845041565d5e1
Damien Vargas <zorille@free.FR>
2020-05-19 08:30:58 +0100

    PlugIn OPNSense SockdIOPS using sockdiops 1.0.1

--- opnsense/sockdiops/src/opnsense/scripts/OPNsense/SockdIOPS/setup.sh
+++ opnsense/sockdiops/src/opnsense/scripts/OPNsense/SockdIOPS/setup.sh
@@ -18,16 +18,8 @@ fi
 #We add our startup script
 cp /usr/local/opnsense/scripts/OPNsense/SockdIOPS/sockdiops /usr/local/etc/rc.d/
 if [ ! -e "/usr/local/sbin/sockdiops" ]; then
-	mv /usr/local/sbin/sockd /usr/local/sbin/sockdiops
-	chmod 555 /usr/local/sbin/sockdiops
-	ln -s /usr/local/sbin/sockdiops /usr/local/sbin/sockd
-else
-    if [ ! -L "/usr/local/sbin/sockd" ]; then
-	rm -f /usr/local/sbin/sockdiops
-	mv /usr/local/sbin/sockd /usr/local/sbin/sockdiops
-	chmod 555 /usr/local/sbin/sockdiops
-	ln -s /usr/local/sbin/sockdiops /usr/local/sbin/sockd
-    fi
+	>&2 echo "sockdiops deamon missing"
+	exit 1
 fi
 
 
