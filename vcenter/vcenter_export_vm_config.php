bc7797e3df5dd66c8ef67c5064c8c20f2bb0a2b3
Damien Vargas <zorille@free.FR>
2021-02-15 18:21:05 +0000

    vcenter : reprise namespace

--- vcenter/vcenter_export_vm_config.php
+++ vcenter/vcenter_export_vm_config.php
@@ -11,6 +11,11 @@ $rep_document = dirname ( $argv [0] ) . "/../..";
  * Permet d'inclure toutes les librairies communes necessaires
  */
 require_once $rep_document . "/php_framework/config.php";
+use Zorille\framework as Core;
+use Zorille\VMware as VMware;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use \Exception as Exception;
 
 /**
  *
@@ -33,8 +38,8 @@ function help() {
 	$help [$fichier] ["text"] [] .= "\t--dossier /tmp Dossier de sortie pour les fichiers xml. /tmp par defaut";
 	
 	$class_utilisees = array ();
-	$help = array_merge ( $help, fonctions_standards::help_fonctions_standard ( false, true, $class_utilisees ) );
-	fonctions_standards::affichage_standard_help ( $help );
+	$help = array_merge ( $help, Core\fonctions_standards::help_fonctions_standard ( false, true, $class_utilisees ) );
+	Core\fonctions_standards::affichage_standard_help ( $help );
 	echo "[Exit]0\n";
 	exit ( 0 );
 }
@@ -44,7 +49,7 @@ if ($liste_option->verifie_option_existe ( "help" ))
 	help ();
 	
 	// Le fichier de log est cree
-abstract_log::onInfo_standard ( "Heure de depart : " . date ( "d/m/Y H:i:s", time () ) );
+Core\abstract_log::onInfo_standard ( "Heure de depart : " . date ( "d/m/Y H:i:s", time () ) );
 
 /**
  * ******** VOTRE CODE A PARTIR D'ICI*********
@@ -56,18 +61,18 @@ function principale(&$liste_option, &$fichier_log) {
 	if ($liste_option->verifie_option_existe ( "dossier", true ) === false) {
 		$liste_option->setOption ( 'dossier', "/tmp" );
 	}
-	$vmware_webservice = vmwareWsclient::creer_vmwareWsclient ( $liste_option, vmwareDatas::creer_vmwareDatas ( $liste_option ) );
+	$vmware_webservice = VMware\vmwareWsclient::creer_vmwareWsclient ( $liste_option, VMware\vmwareDatas::creer_vmwareDatas ( $liste_option ) );
 	if ($vmware_webservice) {
 		try {
 			$vmware_webservice->prepare_connexion ( $liste_option->getOption ( "vmware_utilise" ) );
 		} catch ( Exception $e ) {
-			return abstract_log::onError_standard ( $e->getMessage (), "", $e->getCode () );
+			return Core\abstract_log::onError_standard ( $e->getMessage (), "", $e->getCode () );
 		}
 	} else {
-		return abstract_log::onError_standard ( "Erreur dans les variables necessaires" );
+		return Core\abstract_log::onError_standard ( "Erreur dans les variables necessaires" );
 	}
 	
-	$vim25 = Vim25::creer_Vim25 ( $liste_option, $vmware_webservice );
+	$vim25 = VMware\Vim25::creer_Vim25 ( $liste_option, $vmware_webservice );
 	$liste_VMs = $vim25->Get_VirtualMachine ();
 	foreach ( $liste_VMs as $VM ) {
 		try {
@@ -77,6 +82,9 @@ function principale(&$liste_option, &$fichier_log) {
 			) );
 			if (isset ( $donnees_Vm ['config'] ['name'] )) {
 				$nom = str_replace ( " ", "_", $donnees_Vm ['config'] ['name'] );
+				$nom = str_replace ( "(", "_", $nom );
+				$nom = str_replace ( ")", "_", $nom );
+
 				// On creer un objet SimpleXMLElement
 				$xml_vm_info = new SimpleXMLElement ( "<?xml version=\"1.0\" encoding=\"UTF-8\"?><" . $nom . "/>" );
 				
@@ -86,15 +94,15 @@ function principale(&$liste_option, &$fichier_log) {
 				//On enregistre le fichier de sortie
 				$fichier = $liste_option->getOption ( "dossier" ) . "/" . $nom . ".xml";
 				$texte = str_replace ( "><", ">\n<", $xml_vm_info->asXML () );
-				$file = fichier::creer_fichier ( $liste_option, $fichier, "oui" );
+				$file = Core\fichier::creer_fichier ( $liste_option, $fichier, "oui" );
 				$file->ouvrir ( "w" );
 				$file->ecrit ( $texte );
 				$file->close ();
-				abstract_log::onInfo_standard ( $fichier );
+				Core\abstract_log::onInfo_standard ( $fichier );
 			}
 		} catch ( Exception $e ) {
 			$vmware_webservice->logout ();
-			return abstract_log::onError_standard ( $e->getMessage (), "", $e->getCode () );
+			return Core\abstract_log::onError_standard ( $e->getMessage (), "", $e->getCode () );
 		}
 	}
 	
@@ -102,7 +110,7 @@ function principale(&$liste_option, &$fichier_log) {
 }
 
 principale ( $liste_option, $fichier_log );
-abstract_log::onInfo_standard ( "Heure de fin : " . date ( "d/m/Y H:i:s", time () ) );
+Core\abstract_log::onInfo_standard ( "Heure de fin : " . date ( "d/m/Y H:i:s", time () ) );
 
 exit ( $fichier_log->renvoiExit () );
 ?>
